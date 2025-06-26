a0f7610718f2e560646855567d7d719759d7082c
juliocsalves <juliocsalves@gmail.com>
2025-06-26 10:50:01 -0300

    deploy: build para GitHub Pages

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 811, hash: '90f48a6765c5e27559751c43101b31cefef612fa28fa4aa5e50a6a2cd7e60aa5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1017, hash: '12e5593a38274dc3d3db236e55f648290f4c9fb479e05f3507b86d051eedda7b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 4120180, hash: '93ef29e1608dd4e359d4442d56e0779c709579fc6bc3b2852cb84042774cca61', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 811, hash: '4e1aa189227e055e5fdb67b3c91e892eee3639c0f99f3c92c9794899aea6bc28', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1017, hash: 'cef70323dca2b55363e4ca47dfbe948a3e95b539c62ea0bd9d86c7f37b6884ea', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 4120260, hash: '6c161f00cd4d22ddf05b436922b10e31da29dda0097a702c62ef35855de33af0', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-HOMYPVUV.css': {size: 278, hash: 'llt1eFpClJ4', text: () => import('./assets-chunks/styles-HOMYPVUV_css.mjs').then(m => m.default)}
   },
 };
